121ca4a5d6df0f5f55c7bf58ef7312f16569fabc
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-24 15:08:42 -0700

    created play song function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const sound = document.querySelector("audio");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 const playBtn = document.querySelector("#play");
+
+// Play
+const playSong = () => {
+  sound.play();
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+};
+
+// Pause
