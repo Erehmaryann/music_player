26294103db4d951d9e6e067f8b8bef2b6b821954
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-24 15:03:15 -0700

    selected the fourth element in Js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const sound = document.querySelector("audio");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
+const playBtn = document.querySelector("#play");
